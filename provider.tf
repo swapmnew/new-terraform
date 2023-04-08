308ac70514ca0b67cdca9d12af0a44a400a83563
swapmnew <test@gmail.com>
2023-04-08 16:23:12 +0530

    my updated files

--- provider.tf
+++ provider.tf
@@ -1,17 +1,17 @@
 terraform {
-required_version = ">= 1.3.4"
+  required_version = ">= 1.3.4"
   required_providers {
     openstack = {
-      source = "terraform-provider-openstack/openstack"
+      source  = "terraform-provider-openstack/openstack"
       version = "~> 1.42.0"
     }
   }
 }
 
-#  username     = "${var.username}"
- # password     = "${var.password}"
- # tenant_name  = "${var.tenant_name}"
- # region       = "${var.region}"
+#  username     = "${var.tenant_username}"
+# password     = "${var.tenant_password}"
+# tenant_name  = "${var.tenant_name}"
+# region       = "${var.region}"
 #variable "username" {}
 #variable "password" {}
 #variable "tenant_name" {}
--- variable.tf
+++ variable.tf
@@ -1,9 +1,21 @@
-variable "username" {}
-variable "password" {}
+variable "tenant_username" {}
+variable "tenant_password" {}
 variable "tenant_name" {}
 variable "openstack_auth_url" {}
 
+variable "security_group_names" {
+ type = list(string)
+ default = [ "default" ] 
+}
 
+variable "instance_count" {
+  type = number
+  default = 1
+}
+
+#variable "security_group_names" {}
+variable "INSTANCE_NAME" {}
+variable "VOLUME_NAME" {}
 variable "image_id" {}
 variable "flavor_id" {}
 variable "network_name" {}
@@ -13,3 +25,8 @@ variable "volume_type" {}
 variable "volume_size" {}
 variable "availability_zone" {}
 variable "floating_ip_address" {}
+
+#output "Floating_IP_Of_MY_INSTANCE" {
+#  value = openstack_compute_instance_v2.example_instance.floating_ip
+#}
+
